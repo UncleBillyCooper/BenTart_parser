be70ebcc6153ceedb4b632bcee529d5e15167abe
UncleBillyCooper <119150424+UncleBillyCooper@users.noreply.github.com>
2022-12-01 04:23:56 +0700

    Update benTart.js

--- benTart.js
+++ benTart.js
@@ -1,85 +1,68 @@
+const axios = require("axios");
 const cheerio = require('cheerio');
-const axios = require('axios');
-const fs = require('fs');
+const HttpProxyAgent = require("http-proxy-agent");
+const HttpsProxyAgent = require("https-proxy-agent");
 
 
-const getHTML=axios.get('https://rent-megapark.ru/')
-.then(html=>{
-    const $ = cheerio.load(html.data);
-    
-    //=========================================================
-    const nameJeanHTML=$('.products-holder-info>.name').text().split('\n');
-    const nameJeanArr=[];
-    for (let i=0;i<=24;i++) {
-        if (nameJeanHTML[i]!==''&&nameJeanHTML[i]!=='Swatches'){
-            nameJeanArr.push(nameJeanHTML[i]) 
-        }
-    
-      
-    }
-    //=============================================================
-    const   priceJeanArr=[];
-    const priceJeanHTML=$('.products-holder-info>.price').text().split('\n')//.match(/[\d]+[\.]+[\d]*/g);
-    for (let i=0;i<=55;i++) {
-        if (priceJeanHTML[i]!==''){
-            priceJeanArr.push(priceJeanHTML[i]);
-             
-        }
-        
-    }
-    //=======================================================
-    const   imageJeanArr=[];
-    const   imageJean=[];
-    const imageJeanHTML=$('.products-holder-info>.img');
-    imageJeanHTML.each((i,elem)=>{
-        imageJean.push($(elem).find('img').attr('src'))
-    })
-    for(let i=0;i<=11;i++){
-            if (imageJean[i]!=='assets/images/default.jpg'){
-            imageJeanArr.push(imageJean[i]) 
-        }
-    }
-    imageJeanArr.unshift('NO IMAGE')
-    //==========================================================
-    const totalArray=[]
-
-    function Goodie(name, price,image) {
-        this.name = name;
-        this.price = price;
-        this.image = image;
-    }
-
-    for(i=0;i<=10;i++){
-        const item = new Goodie(nameJeanArr[i], priceJeanArr[i],imageJeanArr[i]);
-        totalArray.push(item)
-        
-        //console.log(arrayNameJean[i]);
-        //console.log(arrayPriceJean[i])
-
-    }
-//==================================================
-
-    //console.log(nameJeanArr)
-    //console.log(priceJeanArr)
-    //console.table(imageJeanArr)
-    //console.log(totalArray)
-    console.log($.html())
-    
-    
+const url = "https://www.tartextextiles.com/Jean-Cloth_c_14.html";
+const proxy = "http://79dbf7702f3666ec31048e7e8239ee9deb619102:@proxy.zenrows.com:8001";
+const httpAgent = new HttpProxyAgent(proxy);
+const httpsAgent = new HttpsProxyAgent(proxy);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+axios({
+	url,
+	httpAgent,
+	httpsAgent,
+	method: "GET",
 })
+    .then(response => {
+        const $=cheerio.load(response.data);
+        //===========================================
+        function myFilter(value) {
+            if (value!=='assets/images/default.jpg'&&value!==''&&value!=='Swatches'){
+              return value
+            }  
+          }
+        //===========================================
+        const nameJeanHTML=$('.products-holder-info>.name').text().split('\n');
+        const nameJeanArr=nameJeanHTML.filter(myFilter);
+        
+        //====================
+        const priceJeanHTML=$('.products-holder-info>.price').text().split('\n');
+        const   priceJeanArr=priceJeanHTML.filter(myFilter);
+        
+        //====================
+        let   imageJeanArr=[];
+        const   imageJean=[];
+        const imageJeanHTML=$('.products-holder-info>.img')
+        .each((i,elem)=>{
+        imageJean.push($(elem).find('img').attr('src'));
+       
+        imageJeanArr=imageJean.filter(myFilter);
+        
 
-// airtable API key keyqBXUjRWwhafmU7
-// appCxZ411GXCRNFkJ
+        //========================================
+        const totalArray=[]
+        let item
 
+        function Goodie(name, price,image) {
+            this.name = name;
+            this.price = price;
+            this.image = image;
+        }
 
-//fetch('https://api.airtable.com/v0/appCxZ411GXCRNFkJ/tblvW2epdPIoA2V8R', {
-        //method: 'POST',
-        //headers: {
-          //'Authorization': 'Bearer keyqBXUjRWwhafmU7',
-          //'Content-Type': 'application/json'
-        //},
-        //body: JSON.stringify(totalArray)
-      //})
-      //.then(response => response.json())
-    
-//})
+        for(i=0;i<=10;i++){
+            item = new Goodie(nameJeanArr[i], priceJeanArr[i],imageJeanArr[i]);
+        
+        }
+        totalArray.push(item)
+        console.log(totalArray)
+        //========================================
+    })
+        //====================
+        //console.log(nameJeanArr)
+        //console.log(priceJeanArr)
+        //console.log(imageJeanArr)
+
+    })
+    .catch(error => console.log(error));
